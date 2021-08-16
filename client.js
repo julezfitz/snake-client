8e6708e6ccb4c7aafc16ff238d84a022acd809bf
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-16 13:43:02 +0000

    coded snake to test movement upwards

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const connect = function(message) {
+const connect = function (message) {
   const conn = net.createConnection({
     host: '10.0.2.15',
     port: 50541
@@ -10,6 +10,17 @@ const connect = function(message) {
   conn.on('connect', () => {
     conn.write('Name: Jul');
   });
+
+  // Test to move snake up every 50ms
+  // conn.on('connect', () => {
+  //   let count = 0;
+  //   while (count < 20) {
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+  //     }, count * 500);
+  //     count += 1;
+  //   }
+  // });
 
   conn.on('data', (data) => {
     console.log('Message from game: ', data);
