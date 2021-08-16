bbbbadafecee86fb7a5eabb291626862021198f5
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-16 13:18:17 +0000

    refractored code to client.js and play.js files with separate functions

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function(message) {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Message from game: ', data);
+  });
+
+  return conn;
+};
+
+let connectObject = {
+  connect
+};
+
+module.exports = connectObject;
